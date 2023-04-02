e18430b3bf01a535622995b83f58b69ba2d797cb
Rexbug <594258774qq.com>
2023-04-02 22:38:30 +0800

    build env 3

---
+++ platform/arm64/imx6ull.cmake
@@ -0,0 +1,6 @@
+
+set(tools /opt/imx6ull/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabihf-g++)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
--- run.sh
+++ run.sh
@@ -1,19 +1,139 @@
 #!/bin/bash
 
+# treat unset var as an error
+set -o nounset
+
+echo_info() { echo -e "\e[32;40m $* \e[0m"; }
+echo_err() { echo -e "\e[41;37m $* \e[0m"; }
+echo_warn() { echo -e "\e[43;37m $* \e[0m"; }
+
+PlatformList=("IMX6ULL");
+PLATFORM=""
+set_platform()
+{
+    for name in ${PlatformList[@]}
+    do
+        if [ ${name} == $1 ]; then
+            PLATFORM=$1
+            echo_info "found platform ${PLATFORM}"
+            return 0;
+        fi
+    done
+    echo_err "ERR! not found any platform called $1"
+    return 1;
+}
+
+ActionList=("all" "lll" "test")
+ACTION="all"
+set_action()
+{
+    for name in ${ActionList[@]}
+    do
+        if [ ${name} == $1 ]; then
+            ACTION=$1
+            echo_info "found action ${ACTION}"
+            return 0;
+        fi
+
+    done
+    echo_err "ERR! not found any action called $1"
+    return 1;
+}
+
+CompileModeList=("release" "debug")
+COMPILE_MODE="debug"
+set_complie_mode()
+{
+    for name in ${CompileModeList[@]}
+    do
+        if [ ${name} == $1 ]; then
+            COMPILE_MODE=$1
+            echo_info "found compilemode is ${COMPILE_MODE}"
+            return 0;
+        fi
+
+    done
+    echo_err "ERR! not found any compile mode called $1"
+    return 1;
+}
+
+tomb()
+{
+    echo_info "$0 -d [release|debug] -p [IMX6ULL] -n [all|lll|test]"
+
+    echo "PLATFORM is ${PLATFORM}"
+    echo "Supported PLATFORM have:"
+    echo_info "${PlatformList[@]}"
+
+    echo "ACTION is ${ACTION}"
+    echo "Supported ACTION have:"
+    echo_info  "${ActionList[@]}"
+
+    echo "COMPILE_MODE is ${COMPILE_MODE}"
+    echo "Supported COMPILE_MODE have:"
+    echo_info  "${CompileModeList[@]}"
+
+    return 0;
+}
+
 main()
 {
-    echo "Start!"
-    echo "The number of parameters is: $#"
-    echo "All parameters are: $@"
+    echo_info "Start!"
+    # echo_info "The number of parameters is: $#"
+    # echo_info "All parameters are: $@"
 
     cmakeCmd="cmake -B build "
     makeCmd="make -C build -j8 "
 
-    if [ "$1" = "all" ]; then
-        rm -rf ./build/*
+    while [ $# -gt 0 ]
+    do
+        case "$1" in
+        -p) set_platform $2;
+            [ $? -eq 1 ] && { tomb; exit 1;}
+            shift 2;;
+        -n) set_action $2;
+            [ $? -eq 1 ] && { tomb; exit 1;}
+            shift 2;;
+        -d) set_complie_mode $2;
+            [ $? -eq 1 ] && { tomb; exit 1;}
+            shift 2;;
+        --) shift
+            break;;
+        *) echo_err "Internel Err! "; exit 1;;
+        esac
+    done
+
+    # 检查参数是否有效
+    [ -z ${PLATFORM} ] || [ -z ${COMPILE_MODE} ] || [ -z ${ACTION} ] && {
+        echo_err "options loss!"
+        [ -z ${PLATFORM} ] && { echo_err "loss platform option"; }
+        [ -z ${COMPILE_MODE} ] && { echo_err "loss compile mode option"; }
+        [ -z ${ACTION} ] && { echo_err "loss action option"; }
+        exit 1;
+    }
+
+    if [ ${COMPILE_MODE} == "release" ]; then
+        cmakeCmd="${cmakeCmd} -DCMAKE_BUILD_TYPE=Release"
+    elif [ ${COMPILE_MODE} == "debug" ]; then
+        cmakeCmd="${cmakeCmd} -DCMAKE_BUILD_TYPE=Debug"
+    else
+        echo_warn "unknown compile mode"
+    fi
+
+    if [ ${PLATFORM} == "IMX6ULL" ]; then
+        cmakeCmd="${cmakeCmd} -DCMAKE_TOOLCHAIN_FILE=./platform/arm64/imx6ull.cmake"
+    else
+        echo_warn "unknown paltform"
+    fi
+    cmakeCmd="${cmakeCmd} -DPLATFORM:STRING=${PLATFORM}"
+
+    echo_info "CMake cmd: ${cmakeCmd}"
+
+    if [ "${ACTION}" = "all" ]; then
+        /usr/bin/rm -rf ./build/*
         ${cmakeCmd}
         ${makeCmd}
-    elif [ "$1" = "lll" ]; then
+    elif [ "${ACTION}" = "lll" ]; then
         ${cmakeCmd}
         ${makeCmd}
     fi
@@ -22,5 +142,5 @@ main()
 
 
 
-main $@
+main $*
 
