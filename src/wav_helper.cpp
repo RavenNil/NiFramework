d95590eb797cc5c5e53feccebea5eb9f4c91867b
linminghao <linminghao@autocruis.com>
2023-12-14 22:15:12 +0800

    update

--- src/wav_helper.cpp
+++ src/wav_helper.cpp
@@ -8,41 +8,119 @@
 
 #include "wav_helper.h"
 
+#include <memory.h>
+
+#include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <memory.h>
 #include <string>
 
 // 读取 WAV 头部信息
-int read_wav_header(const std::string& filename, WAVHeader &header) {
-
+int read_wav_header(const std::string &filename, WAVHeader &header)
+{
     std::ifstream file(filename, std::ios::binary);
-    WAVHeaderPacked raw;
 
     if (!file.is_open()) {
-      std::cout << "Unable to open file: " << filename << std::endl;
-      return -1;
+        std::cout << "Unable to open file: " << filename << std::endl;
+        return -1;
     }
 
+    //判断文件大小是否至少44
+    file.seekg(0, std::ios::end);
+    std::streampos fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+    if (fileSize < 44) {
+        std::cout << "File too small: " << filename << std::endl;
+        file.close();
+        return -1;
+    }
+
+    WAVHeaderPacked raw;
     file.read(reinterpret_cast<char *>(&raw), sizeof(raw));
 
-    header.uriff_header.u32 = raw.u32riff_header_b; // string no need chagne
-    header.u32wav_size = le_to_native(raw.u32wav_size_l);
+    header.uriff_header.u32 = raw.u32riff_header_b;  // string no need chagne
+    header.u32wav_size = le_to_host(raw.u32wav_size_l);
     header.uwave_header.u32 = raw.u32wave_header_b;
 
-    header.ufmt_header.u32 = raw.u32fmt_header_b;
-    header.u32fmt_chunk_size = le_to_native(raw.u32fmt_chunk_size_l);
-    header.u16audio_format = le_to_native(raw.u16audio_format_l);
-    header.u16num_channels = le_to_native(raw.u16num_channels_l);
-    header.u32sample_rate = le_to_native(raw.u32sample_rate_l);
-    header.u32byte_rate = le_to_native(raw.u32byte_rate_l);
-    header.u16sample_alignment = le_to_native(raw.u16sample_alignment_l);
-    header.u16bit_depth = le_to_native(raw.u16bit_depth_l);
+    WAVChunkDescPacked stChunkDescPacked = {};
+    while (file.read(reinterpret_cast<char *>(&stChunkDescPacked), sizeof(stChunkDescPacked))) {
+        struct {
+            union {
+                uint32_t u32;
+                char str[5];
+            } uID = {};
+            uint32_t u32Size;
+        } stChunkDesc;
 
-    header.usub_chunk_2_id.u32 = raw.u32sub_chunk_2_id_b;
-    header.u32sub_chunk_2_size = le_to_native(raw.u32sub_chunk_2_size_l);
+        stChunkDesc.uID.u32 = stChunkDescPacked.u32chunk_header_b;
+        stChunkDesc.uID.str[4] = '\0';
+        stChunkDesc.u32Size = le_to_host(stChunkDescPacked.u32chunk_chunk_size_l);
+        if (0 == strcmp(stChunkDesc.uID.str, "fmt ")) {
+            file.seekg(-8L, std::ios::cur);
+            WAVFmtChunkPacked fmt = {};
+            file.read(reinterpret_cast<char *>(&fmt), sizeof(fmt));
+            // header.u32fmt_chunk_size = le_to_native(fmt.u32fmt_chunk_size_l);
+            header.u16audio_format = le_to_host(fmt.u16audio_format_l);
+            header.u16num_channels = le_to_host(fmt.u16num_channels_l);
+            header.u32sample_rate = le_to_host(fmt.u32sample_rate_l);
+            header.u32byte_rate = le_to_host(fmt.u32byte_rate_l);
+            header.u16sample_alignment = le_to_host(fmt.u16sample_alignment_l);
+            header.u16bit_depth = le_to_host(fmt.u16bit_depth_l);
+        } else if (0 == strcmp(stChunkDesc.uID.str, "data")) {
+            header.u32DataStartPos =  file.tellg();
+            header.u32DataSize = stChunkDesc.u32Size;
+            file.seekg(stChunkDesc.u32Size, std::ios::cur);
+        } else {
+            printf("chunk %s do not care\n", stChunkDesc.uID.str);
+            file.seekg(stChunkDesc.u32Size, std::ios::cur);
+        }
+    }
 
+    file.close();
     return 0;
 }
 
+snd_pcm_format_t trans_format(const uint16_t &u16format, const uint16_t &u16BitsPerSample)
+{
+    enum AudioFormat {
+        PCM = 1,
+        IEEE_FLOAT = 3,
+        // 可以添加其他编码格式的枚举值
+    };
 
+    enum BitDepth {
+        BITS_8 = 8,
+        BITS_16 = 16,
+        BITS_24 = 24,
+        BITS_32 = 32,
+        BITS_64 = 64,
+        // 可以添加其他位深度的枚举值
+    };
+
+    //约定俗称的
+    if (u16format == PCM) {
+        if (u16BitsPerSample == BITS_8) {
+            return SND_PCM_FORMAT_U8;
+        } else if (u16BitsPerSample == BITS_16) {
+            return SND_PCM_FORMAT_S16;
+        } else if (u16BitsPerSample == BITS_24) {
+            return SND_PCM_FORMAT_S24;
+        } else if (u16BitsPerSample == BITS_32) {
+            return SND_PCM_FORMAT_S32;
+        } else {
+            return SND_PCM_FORMAT_UNKNOWN;
+        }
+    } else if (u16format == IEEE_FLOAT) {
+        if (u16BitsPerSample == BITS_32) {
+            return SND_PCM_FORMAT_FLOAT;
+        } else if (u16BitsPerSample == BITS_64) {
+            return SND_PCM_FORMAT_FLOAT64;
+        } else {
+            return SND_PCM_FORMAT_UNKNOWN;
+        }
+    } else {
+        WARN("unknown audio format, default to SND_PCM_FORMAT_UNKNOWN");
+    }
+
+    return SND_PCM_FORMAT_UNKNOWN;
+}
--- src/wav_helper.h
+++ src/wav_helper.h
@@ -6,61 +6,50 @@
   > Created Time: Thu 07 Dec 2023 02:57:13 PM CST
  *****************************************************/
 
+#include "alsa/asoundlib.h"
+#include <endian.h>
+
 #include <string>
 #include <type_traits>
 
 // 将小端数据转换为当前平台的格式（32位）
-template <typename T,
-          std::enable_if_t<std::is_integral<std::decay_t<T>>::value, void *> = nullptr,
+template <typename T, std::enable_if_t<std::is_integral<std::decay_t<T>>::value, void *> = nullptr,
           std::enable_if_t<sizeof(T) == 4, void *> = nullptr>
-T le_to_native(T value) {
-    #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    return value; // 小端平台，不需要转换
-    #else
-    return ((value >> 24) & 0xff) | ((value << 8) & 0xff0000) | ((value >> 8) & 0xff00) | ((value << 24) & 0xff000000);
-    #endif
+T le_to_host(T value)
+{
+    return le32toh(value);
 }
 
 // 将小端数据转换为当前平台的格式（16位）
-template <typename T,
-          std::enable_if_t<std::is_integral<std::decay_t<T>>::value, void *> = nullptr,
+template <typename T, std::enable_if_t<std::is_integral<std::decay_t<T>>::value, void *> = nullptr,
           std::enable_if_t<sizeof(T) == 2, void *> = nullptr>
-T le_to_native(T value) {
-    #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    return value; // 小端平台，不需要转换
-    #else
-    return (value >> 8) | (value << 8);
-    #endif
+T le_to_host(T value)
+{
+    return le16toh(value);
 }
 
 // 将大端数据转换为当前平台的格式（16位）
-template <typename T,
-          std::enable_if_t<std::is_integral<std::decay_t<T>>::value, void *> = nullptr,
+template <typename T, std::enable_if_t<std::is_integral<std::decay_t<T>>::value, void *> = nullptr,
           std::enable_if_t<sizeof(T) == 2, void *> = nullptr>
-T be_to_native(T value) {
-#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-  return value; // 大端平台，不需要转换
-#else
-  return (value >> 8) | (value << 8);
-#endif
+T be_to_host(T value)
+{
+    return be16toh(value);
 }
 // 将大端数据转换为当前平台的格式（32位）
-template <typename T,
-          std::enable_if_t<std::is_integral<std::decay_t<T>>::value, void *> = nullptr,
+template <typename T, std::enable_if_t<std::is_integral<std::decay_t<T>>::value, void *> = nullptr,
           std::enable_if_t<sizeof(T) == 4, void *> = nullptr>
-T be_to_native(T value) {
-#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-  return value; // 大端平台，不需要转换
-#else
-  return ((value >> 24) & 0xff) | ((value << 8) & 0xff0000) |
-         ((value >> 8) & 0xff00) | ((value << 24) & 0xff000000);
-#endif
+T be_to_host(T value)
+{
+    return be32toh(value);
 }
 
 struct WAVHeaderPacked {
   uint32_t u32riff_header_b; // string no need chagne
   uint32_t u32wav_size_l;
   uint32_t u32wave_header_b;
+} __attribute__((packed));
+
+struct WAVFmtChunkPacked {
   uint32_t u32fmt_header_b;
   uint32_t u32fmt_chunk_size_l;
   uint16_t u16audio_format_l;
@@ -69,36 +58,38 @@ struct WAVHeaderPacked {
   uint32_t u32byte_rate_l;
   uint16_t u16sample_alignment_l;
   uint16_t u16bit_depth_l;
-  uint32_t u32sub_chunk_2_id_b;
-  uint32_t u32sub_chunk_2_size_l;
 } __attribute__((packed));
 
-struct WAVHeader{
+struct WAVChunkDescPacked {
+    uint32_t u32chunk_header_b;
+    uint32_t u32chunk_chunk_size_l;
+} __attribute__((packed));
+
+struct WAVHeader {
     union {
       uint32_t u32;
       char str[5];
-    } uriff_header;
-    uint32_t u32wav_size;
+    } uriff_header;//RIFF
+    uint32_t u32wav_size;  //文件长度 -8
     union {
       uint32_t u32;
       char str[5];
-    } uwave_header;
-    union {
-      uint32_t u32;
-      char str[5];
-    } ufmt_header;
-    uint32_t u32fmt_chunk_size ;
-    uint16_t u16audio_format;
-    uint16_t u16num_channels;
-    uint32_t u32sample_rate;
-    uint32_t u32byte_rate;
-    uint16_t u16sample_alignment;
-    uint16_t u16bit_depth;
-    union {
-      uint32_t u32;
-      char str[5];
-    } usub_chunk_2_id;
-    uint32_t u32sub_chunk_2_size;
+    } uwave_header;  // "WAVE"
+
+    //fmt chunk信息
+    uint32_t u32fmt_chunk_size;
+    uint16_t u16audio_format;      //音频格式
+    uint16_t u16num_channels;      //声道数
+    uint32_t u32sample_rate;       //采样率
+    uint32_t u32byte_rate;         //字节比特率
+    uint16_t u16sample_alignment;  //数据块长度 (channel* sample_bit / 8);
+    uint16_t u16bit_depth;         //位深度
+
+    // data chunk信息
+    uint32_t u32DataStartPos;      // data数据开始位置
+    uint32_t u32DataSize;          // PCM音频数据大小
 };
 
 int read_wav_header(const std::string& filename, WAVHeader &header);
+
+snd_pcm_format_t trans_format(const uint16_t &u16format, const uint16_t &u16BitsPerSample);
